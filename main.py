330f3cf8fe906ea5514d5a8520ee0d694d6ab9ac
RyanShea-GitHub <RyanShea96@gmail.com>
2024-11-12 08:06:41 -0500

    broken - cannot figure out why

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import pygame
 from constants import *    
+from player import *
+from circleshape import *
 
 def main():  
     pygame.init()
+    time = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")        
+
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+  
 
     while running:
-        screen.fill("black")
-        pygame.display.flip()
-    
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill("black")
+        Player.draw(screen)
+        pygame.display.flip()
+        dt == time.tick(60) / 1000
+
+ 
 if __name__ == "__main__":
     main()
+        
+
