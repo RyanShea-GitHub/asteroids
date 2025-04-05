78b542362fadbd7dc6c8cebf7d8c7646d119b991
Ryan <ryanshea96@gmail.com>
2025-04-05 01:57:33 -0400

    Working asteroid field

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *    
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidsfield import AsteroidField
 from circleshape import *
 
 def main():  
@@ -10,27 +12,38 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
-    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    AsteroidField()
   
     while running:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        updatable.update(dt)    
+            
+        updatable.update(dt)
+
         screen.fill("black")
-        drawable.draw(screen)
+         
+        for sprite in drawable:
+            sprite.draw(screen)
         
         pygame.display.flip()
 
         dt = time.tick(60) / 1000
         print(f"dt: {dt}")
         print(f"rotation: {player.rotation}")
+        print(updatable, drawable)
 
  
 if __name__ == "__main__":
